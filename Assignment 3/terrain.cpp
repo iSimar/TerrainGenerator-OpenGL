ad169a4345a7da8cde6f5a9c3148adf55e24dd5b
Connor Simpson <connor_simpson08@hotmail.com>
2017-03-09 03:20:03 -0500

    Update terrain.cpp comments with more detail
    Added more detailed comments and provided explanations to those not familiar with OpenGL

--- Assignment 3/terrain.cpp
+++ Assignment 3/terrain.cpp
@@ -30,6 +30,7 @@ terrain::terrain(){
 
 /* user given input terrain constructor */
 terrain::terrain(int size_x_given, int size_z_given, int type_given, int iterations){
+    //create the terrain from the given inputs
     size_x = size_x_given;
     size_z = size_z_given;
     terrainType = type_given;
@@ -44,11 +45,12 @@ terrain::terrain(int size_x_given, int size_z_given, int type_given, int iterati
 
 /* fault algorithm method for creating a height map for the terrain*/
 void terrain::faultAlgorithm(int iterations){
+    //run the algorithm for a predetermined set of times doing the following operations
     for(int i =0; i<iterations; i++){
         
-        float v = static_cast <float> (rand());
-        float a = sin(v);                                //x co-effcient 
-        float b = cos(v);                                //y co-effcient 
+        float v = static_cast <float> (rand());          //create a random floating point number from {0-1}
+        float a = sin(v);                                //take the sin of the number for the x co-effcient 
+        float b = cos(v);                                //take the cos of the number for the y co-effcient 
         float d = sqrt(size_x*size_x + size_z*size_z);   //distance from the furthest point 
         
         //random c value smaller then the distance from the furthest point of the terrain
@@ -73,22 +75,27 @@ void terrain::faultAlgorithm(int iterations){
         }
     }
 }
-/* circles algorithm method for creating a height map for the terrain*/
+/* circles algorithm method for creating a height map for the terrain
+Information on Circle Algorithm for Terrain Generation can be found at
+http://www.lighthouse3d.com/opengl/terrain/index.php?circles */
 void terrain::circlesAlgorithm (int iterations){
-    //intialize min and max height values                           //increase circle height by 3
+    //loop through the algorithm for the given number of iterations
     for(int i =0; i<iterations; i++){
+        //create a circle at an x and y with radius between 1 and 20 using random numbers
         int random_x = rand()%size_x;                         //randomly select x center point for circle
         int random_z = rand()%size_z;                         //randomly select z center point for circle
-        int random_radius = (rand()%20)+1;                   //randomly generate radius integer between 1 - 20
-        float disp = (rand()%12)+1;
-        for(int x = 0; x<size_x; x++){
+        int random_radius = (rand()%20)+1;                   //randomly generate radius integer between 1 - 20 to be the terrainCircleSize
+        float disp = (rand()%12)+1;                         //randomly generate a value of disp between 1 - 12
+        //loop through the entire height map and run the circle algorithm to check if a given x and y is within the circle and
+        //if so apply the algorithm to it to modify the height to the new value
+        for(int x = 0; x<size_x; x++){                      
             for(int z= 0; z<size_z; z++){
-                int dx = x - random_x;
-                int dz = z - random_z;
-                float distance = sqrtf((dx*dx) + (dz*dz));
-                float pd = (distance*2)/random_radius;
-                if (fabs(pd) <= 1.0){
-                    heightMap[x][z] += (disp/2.0) + (cos(pd*3.14)*(disp/2.0));
+                int dx = x - random_x;                      //get the value for tx to determine distance from circle center
+                int dz = z - random_z;                      //get the value for ty to determine distance from circle center
+                float distance = sqrtf((dx*dx) + (dz*dz));  //get the distance from circle center
+                float pd = (distance*2)/random_radius;      //get the value for pd
+                if (fabs(pd) <= 1.0){                       //check if value is within the circle
+                    heightMap[x][z] += (disp/2.0) + (cos(pd*3.14)*(disp/2.0)); //apply circle alorithm to get the new height
                 }
             }
         }
@@ -97,7 +104,10 @@ void terrain::circlesAlgorithm (int iterations){
     //find new max and min height values of the entire map
     minHeight = 0;
     maxHeight = 0;
-    for(int x=0; x<size_x; x++){
+    //loop through every value and compare it to the current highest and lowest values found
+    //values found that are higher or lower become the new high or low
+    //operation is n^2 complexity
+    for(int x=0; x<size_x; x++){                       
         for(int z=0; z<size_z; z++){
             if(heightMap[x][z]<minHeight){
                 minHeight = heightMap[x][z];
@@ -112,23 +122,30 @@ void terrain::circlesAlgorithm (int iterations){
 
 /* draw the terrain according to the height map, given the colour mode and light mode conditions*/
 void terrain::drawTerrain(int lightingMode, int colorMode){
+    //we map out the entire height array and create quads that are drawn to the screen
     for(int x =1; x<size_x-1; x++){
         for(int z=1; z<size_z-1; z++){
-            glBegin(GL_QUADS);
+            glBegin(GL_QUADS); //OpenGL command to begin drawing shapes, in this case quads
             for(int i = 0; i<4; i++){
                 //draw polygon for each point square of the terrain
-                int new_x = x-quadArray[i][0];
-                int new_z  =z-quadArray[i][1];
-                float new_y = heightMap[new_x][new_z];
+                //GL_QUADS draws quadrilaterals in a clockwise direction starting from the bottom left
+                int new_x = x-quadArray[i][0];  //coordinates to draw the quads are already prefined
+                int new_z  =z-quadArray[i][1];  
+                float new_y = heightMap[new_x][new_z]; //height of section acquired from heightmap float array to be 
                 
                 if(lightingMode == OFF){
+                    //we calculate a normal {0-1} for the height of the current segment to use as
+                    //a black to white gradiant to show the peaks of the terrain in white
                     float useColor = (new_y - minHeight)/(maxHeight-minHeight);
                     glColor3f(useColor, useColor, useColor);
                 }
                 else{
                     //if colour mode selected add colour to terrain depending on height
                     if(colorMode == ON){
+                        //we calculate a normal {0-1} for the height of the current segment as seen before
                         float height_r = (new_y - minHeight)/(maxHeight-minHeight);
+                        //we use a different colour scheme for the different algorithms 
+                        //we then assign a colour to the segment based off its height 
                         if(terrainType == CIRCLE){
                             if (height_r>=0.5){
                                 glColor3f(1, 1, 1);
@@ -159,10 +176,10 @@ void terrain::drawTerrain(int lightingMode, int colorMode){
                         }
                     }
                     else{
-                        glColor3fv(twitterColor);
+                        glColor3fv(twitterColor); //default colour if 
                     }
                 }
-                //normal vectors for each polygon
+                //create a normal vector and a vertex out of the values
                 glNormal3fv(normalVectors[new_x][new_z]);
                 glVertex3d(new_x, new_y, new_z);
             }
@@ -172,6 +189,7 @@ void terrain::drawTerrain(int lightingMode, int colorMode){
 }
 /* calculate normal vectors for each polygon of the terrain */
 void terrain::buildNormalVectors(){
+    //define vectors 
     float v1[3];
     float v2[3];
     float v[3];
